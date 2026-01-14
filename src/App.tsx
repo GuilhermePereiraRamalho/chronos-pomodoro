fdbab02b31d8986f178d27355ca0be65deb8ca0b
Guilherme Ramalho <guilherme.p.ramalho@gmail.com>
2026-01-14 10:15:41 +0100

    Creating the logo component with icon and link

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import "./styles/theme.css";
-import "./styles/gobal.css";
-
 import { Heading } from "./components/Heading";
 import { Container } from "./components/Container";
+import { Logo } from "./components/Logo";
+
+import "./styles/theme.css";
+import "./styles/gobal.css";
 
 export function App() {
   return (
     <>
       <Container>
-        <Heading>LOGO</Heading>
+        <Logo />
       </Container>
 
       <Container>
