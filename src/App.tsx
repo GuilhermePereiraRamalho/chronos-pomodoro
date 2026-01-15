90a0ba2c66fe5c7d23301d42eea755ff79a970fd
Guilherme Ramalho <guilherme.p.ramalho@gmail.com>
2026-01-15 09:55:52 +0100

    Creating the homepage form

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,30 @@ export function App() {
         <CountDown />
       </Container>
 
+      <Container>
+        <form action="" className="form">
+          <div className="formRow">
+            <label htmlFor="input">task</label>
+            <input type="text" id="input"/>
+          </div>
+
+          <div className="formRow">
+            <p>
+              In this cycle, <strong>rest</strong> for <strong>5 minutes</strong>.
+            </p>
+          </div>
+
+          <div className="formRow">
+            <p>Cycles</p>
+            <p>0 0 0 0 0 0</p>
+          </div>
+
+          <div className="formRow">
+            <button>Send</button>
+          </div>
+        </form>
+      </Container>
+
     </>
   );
 }
