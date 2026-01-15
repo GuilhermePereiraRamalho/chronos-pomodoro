bf926642d21942f2ef7fb021d04afd78944f81e8
Guilherme Ramalho <guilherme.p.ramalho@gmail.com>
2026-01-15 10:40:06 +0100

    Creating the Cycles component with colored Pomodoro cycle indicators

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import "./styles/gobal.css";
 import { Menu } from "./components/Menu";
 import { CountDown } from "./components/CountDown";
 import { DefaultInput } from "./components/DefaultInput";
+import { Cycles } from "./components/Cycles";
 
 export function App() {
   return (
@@ -41,8 +42,7 @@ export function App() {
           </div>
 
           <div className="formRow">
-            <p>Cycles</p>
-            <p>0 0 0 0 0 0</p>
+            <Cycles />
           </div>
 
           <div className="formRow">
