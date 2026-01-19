478459698ed73d55c7c3cfc19611c9faf1d01d66
Guilherme Ramalho <guilherme.p.ramalho@gmail.com>
2026-01-19 10:26:39 +0100

    GenericHtml component for pages that render HTML

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,4 +1,6 @@
 import { Container } from "../../components/Container";
+import { GenericHtml } from "../../components/GenericHtml";
+import { Heading } from "../../components/Heading";
 import { MainTemplate } from "../../templates/MainTemplate";
 
 export function NotFound() {
@@ -6,7 +8,30 @@ export function NotFound() {
     <>
       <MainTemplate>
         <Container>
-          <p>Page not found</p>
+          <GenericHtml>
+            <Heading>404 - Page Not Found 🚀</Heading>
+            <p>
+              Oops! It looks like the page you’re trying to access doesn’t
+              exist. Maybe it went on vacation, decided to explore the universe,
+              or got lost somewhere between two black holes. 🌌
+            </p>
+            <p>
+              But don’t worry—you’re not lost in space (yet). You can safely go
+              back to the
+              <a href="/">home page</a> or to the <a href="/history">history</a>{" "}
+              — or you can stay here and pretend you’ve discovered a secret page
+              that only the coolest explorers can access. 🧭✨
+            </p>
+            <p>
+              If you think this page should exist (or if you just want to chat
+              about time travel and wormholes), feel free to get in touch.
+              Otherwise, use the menu to return to the real world.
+            </p>
+            <p>
+              In the meantime, here’s something to think about: “If a page
+              doesn’t exist on the internet, did it ever really exist?” 🤔💭
+            </p>
+          </GenericHtml>
         </Container>
       </MainTemplate>
     </>
