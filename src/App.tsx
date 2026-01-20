a0d470fa5d8f4ac01610130805e9519676373278
Guilherme Ramalho <guilherme.p.ramalho@gmail.com>
2026-01-20 10:40:30 +0100

    Global application state using useState and communication via props

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,24 @@ import { Home } from "./pages/Home";
 
 import "./styles/theme.css";
 import "./styles/gobal.css";
+import { useState } from "react";
+import type { TaskStateModel } from "./models/taskStateModel";
+
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formatedSecondsRemaining: "00:00",
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
 
 export function App() {
-  return <Home />;
+  const [state, setState] = useState(initialState);
+
+  return <Home state={state} setState={setState} />;
 }
