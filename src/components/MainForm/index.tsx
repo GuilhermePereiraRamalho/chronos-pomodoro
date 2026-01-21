d5cf2b976aab1db8ed40133dd6a930a485e9c34b
Guilherme Ramalho <guilherme.p.ramalho@gmail.com>
2026-01-21 09:37:28 +0100

    Passing state via props through multiple component levels (prop drilling)

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -3,11 +3,27 @@ import { PlayCircleIcon } from "lucide-react";
 import { DefaultInput } from "../DefaultInput";
 import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
+import type { HomeProps } from "../../pages/Home";
 
 
-export function MainForm() {
+export function MainForm({state, setState}: HomeProps) {
+  function handleClick(){
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: {
+          ...prevState.config,
+          workTime: 34,
+        },
+        formatedSecondsRemaining: '23:34'
+      }
+    })
+  }
   return (
     <form action="" className="form">
+      <div>
+        <button type="button" onClick={handleClick}>Click</button>
+      </div>
       <div className="formRow">
         <DefaultInput
           type="text"
@@ -19,7 +35,7 @@ export function MainForm() {
 
       <div className="formRow">
         <p>
-          In this cycle, <strong>rest</strong> for <strong>5 minutes</strong>.
+          In this cycle, <strong>rest</strong> for <strong>{state.config.workTime} minutes</strong>.
         </p>
       </div>
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,23 +4,21 @@ import { MainForm } from "../../components/MainForm";
 import type { TaskStateModel } from "../../models/taskStateModel";
 import { MainTemplate } from "../../templates/MainTemplate";
 
-type HomeProps = {
+export type HomeProps = {
   state: TaskStateModel;
   setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
 };
 
-
 export function Home(props: HomeProps) {
-
   return (
     <>
       <MainTemplate>
         <Container>
-          <CountDown />
+          <CountDown {...props}/>
         </Container>
 
         <Container>
-          <MainForm />
+          <MainForm {...props}/>
         </Container>
       </MainTemplate>
     </>
