fec59d8e15710675d250e8faaa29c752c2d7cc70
Guilherme Ramalho <guilherme.p.ramalho@gmail.com>
2026-01-23 09:46:23 +0100

    Working with state before and after setState (getNextCycle)

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -5,10 +5,14 @@ import { DefaultButton } from "../DefaultButton";
 import { useRef } from "react";
 import type { TaskModel } from "../../models/taskModel";
 import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { getNextCycle } from "../../utils/getNextCycle";
 
 export function MainForm() {
-  const { setState } = useTaskContext();
+  const { state, setState } = useTaskContext();
   const taskNameInput = useRef<HTMLInputElement>(null);
+
+  const nextCycle = getNextCycle(state.currentCycle);
+  console.log(nextCycle);
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>): void {
     event.preventDefault();
