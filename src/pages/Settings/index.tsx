0c57dea5af1f09023aa00c1129cb8e0256ed9884
Guilherme Ramalho <guilherme.p.ramalho@gmail.com>
2026-02-05 10:18:09 +0100

    Validating the form fields before saving the data

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -6,6 +6,7 @@ import { Heading } from "../../components/Heading";
 import { MainTemplate } from "../../templates/MainTemplate";
 import { useRef } from "react";
 import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { showMessage } from "../../adapters/showMessage";
 
 export function Settings() {
   const { state } = useTaskContext();
@@ -15,12 +16,38 @@ export function Settings() {
 
   function handleSaveSettings(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
+    showMessage.dismiss();
 
-    const workTime = workTimeInput.current?.value;
-    const shortBreakTime = shortBreakTimeInput.current?.value;
-    const longBreakTime = longBreakTimeInput.current?.value;
+    const formErrors = [];
 
-    console.log(workTime, shortBreakTime, longBreakTime);
+    const workTime = Number(workTimeInput.current?.value);
+    const shortBreakTime = Number(shortBreakTimeInput.current?.value);
+    const longBreakTime = Number(longBreakTimeInput.current?.value);
+
+    if (isNaN(workTime) || isNaN(shortBreakTime) || isNaN(longBreakTime)) {
+      formErrors.push("Enter numbers only for ALL fields");
+    }
+
+    if (workTime < 1 || workTime > 99){
+      formErrors.push("Enter values between 1 and 99 for focus");
+    }
+
+    if (shortBreakTime < 1 || shortBreakTime > 30){
+      formErrors.push("Enter values between 1 and 30 for short break");
+    }
+
+    if (longBreakTime < 1 || longBreakTime > 60){
+      formErrors.push("Enter values between 1 and 60 for long break");
+    }
+
+    if (formErrors.length > 0) {
+      formErrors.forEach((error) => {
+        showMessage.error(error);
+      });
+      return;
+    }
+
+    console.log("SAVE");
   }
   return (
     <>
@@ -43,6 +70,7 @@ export function Settings() {
                 labelText="Focus"
                 ref={workTimeInput}
                 defaultValue={state.config.workTime}
+                type="number"
               />
             </div>
             <div className="formRow">
@@ -51,6 +79,7 @@ export function Settings() {
                 labelText="Short Break"
                 ref={shortBreakTimeInput}
                 defaultValue={state.config.shortBreakTime}
+                type="number"
               />
             </div>
             <div className="formRow">
@@ -59,6 +88,7 @@ export function Settings() {
                 labelText="Long Break"
                 ref={longBreakTimeInput}
                 defaultValue={state.config.longBreakTime}
+                type="number"
               />
             </div>
             <div className="formRow">
